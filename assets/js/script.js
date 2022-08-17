2b36f32ab4998ca57865106731904be30a9cf99e
drewrvaz <drewrvaz@gmail.com>
2022-08-17 00:36:59 -0400

    Swapped the data time with the id in html. Removed some code in the script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,3 @@
 // Variable to set the day and date at the top of the page
 var currentDay = moment();
-$("#currentDay").text(currentDay.format("dddd, MMMM do, YYYY"));
-
-var saveBtn = document.getElementById("#saveBtn")
-var descritpion = $("#description")
-
-var currentHour = 
-
-saveBtn.addEventListener("click", storeDescription)
-console.log("click")
-function storeDescription() {
-  localStorage.setItem("description", JSON.stringify(descritpion))
-}
+$("#currentDay").text(currentDay.format("dddd, MMMM Do YYYY"));
