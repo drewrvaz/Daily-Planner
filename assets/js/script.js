bbe7193470ba6d3e02cccf14c907a5f20ed15e18
drewrvaz <drewrvaz@gmail.com>
2022-08-18 18:26:20 -0400

    Deleted some unecessary elements in the HTML. Added a function to change the color of the area where the text goes. Added a click event to save the item that the user enters

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,18 +8,37 @@ var digitalClock = function() {
 digitalClock();
 setInterval(digitalClock, 1000);
 
+$(document).ready(function() {
+  trackTime();
+
+  $("#hour9 .description").val(localStorage.getItem("hour9"));
+  $("#hour10 .description").val(localStorage.getItem("hour10"));
+  $("#hour11 .description").val(localStorage.getItem("hour11"));
+  $("#hour12 .description").val(localStorage.getItem("hour12"));
+  $("#hour13 .description").val(localStorage.getItem("hour13"));
+  $("#hour14 .description").val(localStorage.getItem("hour14"));
+  $("#hour15 .description").val(localStorage.getItem("hour15"));
+  $("#hour16 .description").val(localStorage.getItem("hour16"));
+  $("#hour17 .description").val(localStorage.getItem("hour17"));
+
+})
+
 function trackTime() {
   var currentHour = moment().hour();
 
-  $(".description"). each(function() {
+  $(".description").each(function() {
     var descriptionHour = parseInt($(this).attr("id"));
 
     if (descriptionHour === currentHour) {
-      $(this).addClass("current");
-    }else if (currentHour < descriptionHour) {
-      $(this).addClass("past")
+      $(this).addClass("present");
+    }else if (descriptionHour < currentHour) {
+      $(this).addClass("past");
     }else {
-      $(this).addClass("future")
+      $(this).addClass("future");
     }
   })
 }
+
+$(".saveBtn").on("click", function() {
+  localStorage.setItem($(this).parent().attr("id"), $(this).prev().val());
+})
