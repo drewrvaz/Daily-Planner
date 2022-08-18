4940ffac5673bbe2b8aa82dfe020121d3cd1822a
drewrvaz <drewrvaz@gmail.com>
2022-08-18 15:24:50 -0400

    Used google to find a more efficient to code with JQuery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,25 @@
-// Variable to set the day and date at the top of the page
-var currentDay = moment();
-$("#currentDay").text(currentDay.format("dddd, MMMM Do YYYY"));
+// 
+var digitalClock = function() {
+  // Variable to set the day and date at the top of the page
+  var currentDay = moment();
+  $("#currentDay").text(currentDay.format("dddd, MMMM Do, YYYY"));
+};
+
+digitalClock();
+setInterval(digitalClock, 1000);
+
+function trackTime() {
+  var currentHour = moment().hour();
+
+  $(".description"). each(function() {
+    var descriptionHour = parseInt($(this).attr("id"));
+
+    if (descriptionHour === currentHour) {
+      $(this).addClass("current");
+    }else if (currentHour < descriptionHour) {
+      $(this).addClass("past")
+    }else {
+      $(this).addClass("future")
+    }
+  })
+}
