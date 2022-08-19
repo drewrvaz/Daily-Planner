58cd509e6967e84bcd39b64174c3e677e6442b8a
drewrvaz <drewrvaz@gmail.com>
2022-08-18 22:30:05 -0400

    Cann't figure it out right now. Going to try one more thing and then call it a night. At least it saves inputs now

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,3 @@
-// var digitalClock = function() {
-//   // Variable to set the day and date at the top of the page
-//   var currentDay = moment();
-//   $("#currentDay").text(currentDay.format("dddd, MMMM Do, YYYY"));
-// };
-
-// digitalClock();
-// 
-
 var hour9 = $("#9");
 var hour10 = $("#10");
 var hour11 = $("#11");
@@ -59,43 +50,3 @@ function trackTime() {
     }
   })
 } console.log(trackTime)
-
-
-// Function to save and call the values from local storage for the user
-// $(document).ready(function() {
-//   trackTime();
-
-//   $("#hour9 .description").val(localStorage.getItem("hour9"));
-//   $("#hour10 .description").val(localStorage.getItem("hour10"));
-//   $("#hour11 .description").val(localStorage.getItem("hour11"));
-//   $("#hour12 .description").val(localStorage.getItem("hour12"));
-//   $("#hour13 .description").val(localStorage.getItem("hour13"));
-//   $("#hour14 .description").val(localStorage.getItem("hour14"));
-//   $("#hour15 .description").val(localStorage.getItem("hour15"));
-//   $("#hour16 .description").val(localStorage.getItem("hour16"));
-//   $("#hour17 .description").val(localStorage.getItem("hour17"));
-
-// })
-
-// // Tracks the current time so that it can be compared to change the background color of the input areas
-// function trackTime() {
-//   var currentHour = moment().hour();
-
-//   // Function to change the background color of the description area based on the time of day
-//   $(".description").each(function() {
-//     var descriptionHour = parseInt($(this).attr("id"));
-
-//     // Changes the color depending on past, present, or future
-//     if (descriptionHour === currentHour) {
-//       $(this).addClass("present");
-//     }else if (descriptionHour < currentHour) {
-//       $(this).addClass("past");
-//     }else {
-//       $(this).addClass("future");
-//     }
-//   })
-// }
-
-// // When you click the save button, the user input is saved in local storage
-// 
-// })
