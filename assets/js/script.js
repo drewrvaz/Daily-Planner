62f82b706586f8ddb8a81d204d2c88b5190ac69a
drewrvaz <drewrvaz@gmail.com>
2022-08-20 12:08:37 -0400

    Added notes to the js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Variables for each of the hours that appear on the daily planner
 var hour9 = $("#9");
 var hour10 = $("#10");
 var hour11 = $("#11");
@@ -7,15 +8,19 @@ var hour2 = $("#14");
 var hour3 = $("#15");
 var hour4 = $("#16");
 var hour5 = $("#17");
+// Variable to call the time and date on the calendar from moment.js
 currentDay = moment();
 var digitalClock = function() {
   var currentDay = moment();
 }
 
+// Function to save the information that the user inputs into the calendar
 function setDailyCalendar() {
-  $("#currentDay").text(currentDay.format("dddd, MMMM Do, YYYY"));
+  // Sets the current date and time on the calendar
+  $("#currentDay").text(currentDay.format("dddd, MMMM Do, YYYY, h:mm:ss"));
   setInterval(digitalClock, 1000);
 
+  // Checks each of the time blocks for an input
   $(".time-block").each(function() {
     var id = $(this).attr("id");
     var hours = localStorage.getItem(id);
@@ -29,6 +34,7 @@ function setDailyCalendar() {
 setDailyCalendar();
 var saveBtn = $(".saveBtn");
 
+// Event listener for the save button that saves the input in local storage
 $(".saveBtn").on("click", function() {
   var time = ($(this).parent().attr("id"));
   var schedule = $(this).siblings(".description").val();
@@ -36,6 +42,7 @@ $(".saveBtn").on("click", function() {
   localStorage.setItem(time, schedule);
 });
 
+// Function to change the color of the input area depending on the time. Currently broken
 function trackTime() {
   hour = moment().hour();
   $(".time-block").each(function() {
